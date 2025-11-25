df70d865f611acdd891dd198d7eda46dab5ca22b
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-25 13:55:26 +0100

    Implemented CRUD routes for /api/posts

--- server.js
+++ server.js
@@ -4,9 +4,35 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
+// index
+app.get('/api/posts', (req, res) => {
+    res.send('Lista dei posts');
+});
 
-})
+// show
+app.get('/api/posts/:id', (req, res) => {
+    res.send('Dettagli del post');
+});
+
+// store
+app.post('/api/posts', (req, res) => {
+    res.send('Creazione nuovo post');
+});
+
+// update
+app.put('/api/posts/:id', (req, res) => {
+    res.send('Modifica del post');
+});
+
+// modify
+app.patch('/api/posts/:id', (req, res) => {
+    res.send('Piccola modifica del post');
+});
+
+// destroy
+app.delete('/api/posts/:id', (req, res) => {
+    res.send('Rimozione post');
+});
 
 
 app.listen(PORT, () => {
